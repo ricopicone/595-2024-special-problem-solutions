93e01caf9eab612e499eef4f82a34a8064b0244a
Rico Picone <rpicone@stmartin.edu>
2024-07-30 16:46:32 -0700

    Added animation and truncating the gradients now

--- reinforcement-learning-cart-pole/main.py
+++ reinforcement-learning-cart-pole/main.py
@@ -14,6 +14,7 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import gymnasium as gym
 import tensorflow as tf
 import time
@@ -90,8 +91,8 @@ def value_network_com(optimizer):
 # Define the optimizers for the policy and value networks:
 
 #%%
-optimizer_policy = tf.keras.optimizers.Adam(learning_rate=0.001)
-optimizer_value = tf.keras.optimizers.Adam(learning_rate=0.001)
+optimizer_policy = tf.keras.optimizers.Adam(learning_rate=0.0001)
+optimizer_value = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
 #%% [markdown]
 # Define the policy and value networks:
@@ -135,7 +136,7 @@ def updater(policy_network, value_network, state, action, state_next, reward, ga
 
         # Losses
         action_prob = policy_network(state)[0, action]  # Prob of action taken
-        log_prob = tf.math.log(action_prob)  # Log prob of action taken
+        log_prob = tf.math.log(action_prob)  # Log prob of action taken (negative)
         policy_loss = -log_prob * td_error  # Policy loss
         value_loss = tf.square(td_error)  # Value loss (MSE)
 
@@ -146,6 +147,10 @@ def updater(policy_network, value_network, state, action, state_next, reward, ga
         value_grads = tape.gradient(
             value_loss, value_network.trainable_variables
         )  # Gradient of the loss function wrt the value network parameters
+
+        # Clip gradients
+        policy_grads = tf.clip_by_global_norm(policy_grads, 10.0)[0]
+        value_grads = tf.clip_by_global_norm(value_grads, 10.0)[0]
 
         # Apply gradients
         optimizer_policy.apply_gradients(
@@ -158,11 +163,99 @@ def updater(policy_network, value_network, state, action, state_next, reward, ga
     return policy_network, value_network
 
 #%% [markdown]
+# Define an animation class to plot a sample of the policy network for the pole (pendulum) angle as follows:
+
+#%%
+class angle_policy_animation:
+    def __init__(self, policy_network, n_points=51, refresh_interval=1000):
+        self.policy_network = policy_network
+        self.n_points = n_points
+        self.refresh_interval = refresh_interval
+        fig, ax = plt.subplots()
+        self.fig = fig
+        self.ax = ax
+        self.angles = np.linspace(-0.418, 0.418, n_points)  # Pole angles
+        self.barcollection_left = None
+        self.barcollection_right = None
+        self.initialize_plot()
+
+    def initialize_plot(self):
+        self.fig, self.ax = plt.subplots()
+        left_probs = np.zeros((self.n_points))
+        right_probs = np.zeros((self.n_points))
+        for i in range(self.n_points):
+            state = np.array([0, self.angles[i], 0, 0])
+            prob = self.policy_network.predict(state.reshape((1, -1)))[0]
+            left_probs[i] = prob[0]
+            right_probs[i] = prob[1]
+        self.barcollection_left = self.ax.bar(self.angles, left_probs, width=0.01, color='b')
+        self.barcollection_right = self.ax.bar(self.angles, right_probs, width=0.01, bottom=left_probs, color='r')
+        self.ax.set_xlabel('Pole Angle')
+        self.ax.set_ylabel('Probability')
+        self.ax.legend(['Push Left', 'Push Right'])
+    
+    def update(self, frame):
+        left_probs = np.zeros((self.n_points))
+        right_probs = np.zeros((self.n_points))
+        for i in range(self.n_points):
+            state = np.array([0, self.angles[i], 0, 0])
+            prob = self.policy_network.predict(state.reshape((1, -1)))[0]
+            left_probs[i] = prob[0]
+            right_probs[i] = prob[1]
+        self.barcollection_left = self.ax.bar(self.angles, left_probs, width=0.01, color='b')
+        self.barcollection_right = self.ax.bar(self.angles, right_probs, width=0.01, bottom=left_probs, color='r')
+        return self.barcollection_left, self.barcollection_right
+    
+    def animate(self):
+        anim = FuncAnimation(self.fig, self.update, frames=None, interval=self.refresh_interval,
+                             blit=False)
+        return anim
+
+#%% [markdown]
+# Define a similar animation class to plot a sample of the value network for the pole (pendulum) angle as follows:
+
+#%%
+class angle_value_animation:
+    def __init__(self, value_network, n_points=51, refresh_interval=1000):
+        self.value_network = value_network
+        self.n_points = n_points
+        self.refresh_interval = refresh_interval
+        fig, ax = plt.subplots()
+        self.fig = fig
+        self.ax = ax
+        self.angles = np.linspace(-0.418, 0.418, n_points)  # Pole angles
+        self.values = np.zeros((n_points))
+        self.initialize_plot()
+
+    def initialize_plot(self):
+        self.fig, self.ax = plt.subplots()
+        for i in range(self.n_points):
+            state = np.array([0, self.angles[i], 0, 0])
+            self.values[i] = self.value_network(state.reshape(1, -1)).numpy()[0][0]
+        self.ax.plot(self.angles, self.values)
+        self.ax.set_xlabel('Pole Angle')
+        self.ax.set_ylabel('Value')
+    
+    def update(self, frame):
+        self.ax.clear()
+        for i in range(self.n_points):
+            state = np.array([0, self.angles[i], 0, 0])
+            self.values[i] = self.value_network(state.reshape(1, -1)).numpy()[0][0]
+        self.ax.plot(self.angles, self.values)
+        return self.ax
+    
+    def animate(self):
+        anim = FuncAnimation(self.fig, self.update, frames=None, interval=self.refresh_interval,
+                             blit=False)
+        return anim
+
+
+#%% [markdown]
 #
 # Define a function to train the policy and value networks using a temporal difference TD(0) method as follows:
 
 #%%
-def train_policy_value_networks(env, policy_network, value_network, updater, n_episodes=1000, gamma=0.99, max_episode_steps=100, print_=False):
+def train_policy_value_networks(env, policy_network, value_network, updater, n_episodes=1000, gamma=0.99, max_episode_steps=100, plot=False, print_=False):
     """Train policy and value networks using a temporal difference TD(0) method.
 
     Update the policy and value networks using the updater function.
@@ -180,10 +273,20 @@ def train_policy_value_networks(env, policy_network, value_network, updater, n_e
     rewards: Numpy array of episode rewards
     """
     rewards = []
+    if plot:
+        animation_policy = angle_policy_animation(policy_network).animate()
+        animation_value = angle_value_animation(value_network).animate()
     for episode in range(n_episodes):
+        if plot:
+            animation_policy.policy_network = policy_network
+            plt.figure(animation_policy._fig.number)
+            plt.pause(0.2)
+            animation_value.value_network = value_network
+            plt.figure(animation_value._fig.number)
+            plt.pause(0.2)
         state, _ = env.reset()  # Reset env (random initial state)
         episode_rewards = []  
-        for step in range(max_episode_steps):
+        for _ in range(max_episode_steps):
             prob = policy_network.predict(state.reshape((1, -1)))[0]
             action = np.random.choice(env.action_space.n, p=prob)
             state_new, reward, term, _, _ = env.step(action)
@@ -203,7 +306,8 @@ def train_policy_value_networks(env, policy_network, value_network, updater, n_e
 #%%
 policy_network, value_network, rewards = train_policy_value_networks(
     env, policy_network, value_network, updater, 
-    n_episodes=100, gamma=0.99, max_episode_steps=600, print_=True
+    n_episodes=100, gamma=0.99, max_episode_steps=600, 
+    plot=True, print_=True
 )
 
 #%% [markdown]
@@ -219,11 +323,32 @@ plt.ylabel('Reward')
 plt.draw()
 
 #%% [markdown]
+# Plot a sample of the policy network for the pole (pendulum) angle as follows:
+
+#%%
+fig, ax = plt.subplots()
+angles = np.linspace(-0.418, 0.418, 101)  # Pole angles
+probs_left = np.zeros((101))  # Probability of pushing left
+probs_right = np.zeros((101))  # Probability of pushing right
+for i in range(len(angles)):
+    state = np.array([0, angles[i], 0, 0])  # State
+    prob = policy_network.predict(state.reshape((1, -1)))[0]  # Probabilities
+    probs_left[i] = prob[0]
+    probs_right[i] = prob[1]
+ax.bar(angles, probs_left, width=0.01, color='b')
+ax.bar(angles, probs_right, width=0.01, bottom=probs_left, color='r')
+ax.set_xlabel('Pole Angle')
+ax.set_ylabel('Probability')
+ax.legend(['Push Left', 'Push Right'])
+plt.draw()
+
+#%% [markdown]
 # Plot a sample of the value network for the pole (pendulum) angle as follows:
 
 #%%
+fig, ax = plt.subplots()
 angles = np.linspace(-0.418, 0.418, 101)  # Pole angles
-values = np.zeros((len(angles)))  # Value
+values = np.zeros((101))  # Value
 for i in range(len(angles)):
     state = np.array([0, angles[i], 0, 0])  # State
     values[i] = value_network(state.reshape(1, -1)).numpy()[0][0]
@@ -231,17 +356,4 @@ fig, ax = plt.subplots()
 ax.plot(angles, values)
 ax.set_xlabel('Pole Angle')
 ax.set_ylabel('Value')
-plt.draw()
-
-#%% [markdown]
-# Plot a sample of the policy network for the pole (pendulum) angle as follows:
-
-#%%
-plt.figure()
-for i in range(len(angles)):
-    state = np.array([0, angles[i], 0, 0])  # State
-    prob = policy_network.predict(state.reshape((1, -1)))[0]
-    plt.plot([angles[i], angles[i]], [0, prob[1]], 'b')
-plt.xlabel('Pole Angle')
-plt.ylabel('Probability of Pushing Right')
 plt.show()
